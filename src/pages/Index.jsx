6904fcaedae44c49952747f4d016e1d7b68e4499
jjwahnon <joshwahnon@gmail.com>
2024-10-06 21:04:55 +0100

    props are not taken in on the Index Component

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -7,8 +7,8 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 
 class Index extends Component{
-    constructor(props){
-        super(props);
+    constructor(){
+        super();
         this.state={
             columns:[],
             countries:[],
