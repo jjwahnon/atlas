c08bcbd5709d48fca2c65e46aae804d08fe48f61
jjwahnon <joshwahnon@gmail.com>
2024-10-07 21:47:55 +0100

    collected country after the refresh

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -11,19 +11,19 @@ import "ag-grid-community/styles/ag-theme-quartz.css";
 class Index extends Component{
     constructor(){
         super();
+        const storedSelectedCountry = localStorage.getItem('selectedCountry');
         this.state={
             columns:[],
             countries:[],
             loading:true,
             selectedRows:[],
-            selectedCountry:[]
+            selectedCountry: storedSelectedCountry || null
         }
     }
 
     async componentDidUpdate(prevProps, prevState){
-        if(prevState.selectedCountry !== this.state.selectedCountry && this.state.selectedCountry.length !== 0){
-            let data= await fetchCountry(this.state.selectedCountry[0]);
-            console.log("country data: ", data);
+        if(prevState.selectedCountry !== this.state.selectedCountry){
+            localStorage.setItem('selectedCountry', this.state.selectedCountry)
         }
     }
 
@@ -58,7 +58,10 @@ class Index extends Component{
     handleSelectionChanged = () =>{
         const selectedRows = this.gridApi.getSelectedRows();
         const selectedCountry=selectedRows.map(country => country.name);
-        this.setState({ selectedRows, selectedCountry });
+        this.setState({ 
+            selectedRows: selectedRows, 
+            selectedCountry: selectedCountry[0] 
+        });
         console.log("selected rows: ", selectedRows);
         console.log("Selected country names: ", selectedCountry);
     }
@@ -68,14 +71,15 @@ class Index extends Component{
         const{loading, countries, columns, selectedRows, selectedCountry}=this.state;
         console.log("countries state: ", this.state.countries);
         console.log("selected countries: ", selectedRows);
+        console.log("selected nation state: ", selectedCountry);
         if (!loading){
             //console.log("selected country: ", selectedCountry.pop());
             return(
                 <div className="flex flex-col justify-center items-center space-y-4">
                     {
-                        (selectedCountry[0])&&
+                        (selectedCountry)&&
                             <div className="border-2 rounded-lg border-blue-400 bg-blue-200 px-8 py-4">
-                                <h1 className="text-2xl font-bold">The selected country is {selectedCountry[0]}</h1>
+                                <h1 className="text-2xl font-bold">The selected country is {selectedCountry}</h1>
                             </div>
                         
                     }
