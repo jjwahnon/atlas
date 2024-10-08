dae768d4d37dab36cf0d3ead78e0885468dac996
jjwahnon <joshwahnon@gmail.com>
2024-10-08 11:35:25 +0100

    added for no captial city

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -93,8 +93,10 @@ class Index extends Component{
                             <div className="border-2 rounded-lg border-blue-400 bg-blue-200 px-8 py-4 d-flex space-y-2">
                                 <h1 className="text-2xl font-bold">The selected country is {selectedCountry.name}</h1>
                                 <ul className="justify-center items-center">
-                                    {selectedCountry['capital city'].length>1?
+                                    {(selectedCountry['capital city'] && selectedCountry['capital city'].length>1)?
                                         (<li>Their capital cities are: <b>{selectedCountry['capital city'].slice(0, -1).join(" , ") + " and "+selectedCountry['capital city'][selectedCountry['capital city'].length-1]}</b></li>):
+                                        (!selectedCountry['capital city'])?
+                                        (<li> There is no official capital city here</li>):
                                         (<li>The capital city is <b>{selectedCountry['capital city'][0]}</b>.</li>)
                                     }
                                     <li>They have a population of <b>{selectedCountry.population}</b> people.</li>
